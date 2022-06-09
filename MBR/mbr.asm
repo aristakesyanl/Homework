5b7c95a2fa13e0015ead9d445f159cf424c9cabd
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-06-09 16:50:12 +0400

    Update mbr.asm

--- MBR/mbr.asm
+++ MBR/mbr.asm
@@ -1,18 +1,22 @@
 [bits 16]
 [org 0x7C00]
+address equ 0x9000
 
 mov bx, message
 call print
 call print_nl
 
 mov bx, 0x9000 ; program is going to be loaded at the address 0x9000
-mov dh, 2 ; read 2 sectors
+mov dh, 1 ; read 1 sector
 call disk_load
+
+jmp address
 
 jmp $
 
 %include "print.asm"
 %include "diskload.asm"
+
 
 message: db 'MBR Loaded!!!', 0
 
